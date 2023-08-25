2c0682f30bd28fd09f9a0409f25f128782145e22
240829120 <240829120@qq.com>
2023-08-25 18:22:42 +0800

    项目初始化

---
+++ Dance.Tool/Dance.Tool.Module/Module/Welcome/WelcomeView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dance.Tool.Module
+{
+    /// <summary>
+    /// 欢迎视图
+    /// </summary>
+    public class WelcomeView : Control
+    {
+        static WelcomeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WelcomeView), new FrameworkPropertyMetadata(typeof(WelcomeView)));
+        }
+    }
+}
---
+++ Dance.Tool/Dance.Tool.Module/Module/Welcome/WelcomeViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dance.Tool.Module
+{
+    /// <summary>
+    /// 欢迎视图模型
+    /// </summary>
+    public class WelcomeViewModel : DanceViewModel
+    {
+        public WelcomeViewModel()
+        {
+            // 初始化命令
+            this.InitCommand();
+        }
+
+        /// <summary>
+        /// 初始化命令
+        /// </summary>
+        private void InitCommand()
+        {
+            this.EnterCommand = new RelayCommand(this.Enter);
+        }
+
+        // ====================================================================================
+        // Property
+
+
+
+        // ====================================================================================
+        // Command
+
+        #region EnterCommand -- 进入命令
+
+        /// <summary>
+        /// 进入命令
+        /// </summary>
+        public RelayCommand? EnterCommand { get; set; }
+
+        /// <summary>
+        /// 进入
+        /// </summary>
+        private void Enter()
+        {
+
+        }
+
+        #endregion
+    }
+}
