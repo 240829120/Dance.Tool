96531c78d63fe2549435f18506ac141e554c021d
240829120 <240829120@qq.com>
2023-10-17 18:29:58 +0800

    添加webview2

---
+++ Dance.Tool/Dance.Tool.Plugin/WebView2/WebView2PluginLifescope.cs
@@ -0,0 +1,32 @@
+﻿using Dance.Tool.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dance.Tool.Plugin
+{
+    /// <summary>
+    /// 插件生命周期 -- WebView2
+    /// </summary>
+    public class WebView2PluginLifescope : DanceObject, IDancePluginLifescope
+    {
+        /// <summary>
+        /// 注册插件
+        /// </summary>
+        /// <returns>插件信息</returns>
+        public IDancePluginInfo Register()
+        {
+            return new PluginModel("WebView2", typeof(WebView2View));
+        }
+
+        /// <summary>
+        /// 初始化插件
+        /// </summary>
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ Dance.Tool/Dance.Tool.Plugin/WebView2/WebView2ViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Dance.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dance.Tool.Plugin
+{
+    /// <summary>
+    /// WebView2 视图模型
+    /// </summary>
+    [DanceSingleton]
+    public class WebView2ViewModel : DanceViewModel
+    {
+        public WebView2ViewModel()
+        {
+            this.GotoCommand = new RelayCommand(this.Goto);
+        }
+
+        // ============================================================================
+        // Property
+
+        #region Url -- 地址
+
+        private string? url = "http://www.baidu.com";
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string? Url
+        {
+            get { return url; }
+            set { url = value; this.OnPropertyChanged(); }
+        }
+
+        #endregion
+
+        // ============================================================================
+        // Command
+
+        #region GotoCommand -- 跳转命令
+
+        /// <summary>
+        /// 跳转命令
+        /// </summary>
+        public RelayCommand? GotoCommand { get; set; }
+
+        /// <summary>
+        /// 跳转
+        /// </summary>
+        private void Goto()
+        {
+            if (this.View is not WebView2View view)
+                return;
+
+            view.web.Source = string.IsNullOrWhiteSpace(this.Url) ? null : new Uri(this.Url);
+        }
+
+        #endregion
+    }
+}
