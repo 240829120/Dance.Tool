399f6c0ac18dde2e78e5b18ac40b01cfa70e9654
240829120 <240829120@qq.com>
2023-10-17 15:31:32 +0800

    修改示例

--- Dance.Tool/Dance.Tool.Plugin/V8/Script/index.js
+++ Dance.Tool/Dance.Tool.Plugin/V8/Script/index.js
@@ -21,14 +21,14 @@ function bubbleSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
     }
 }
 
 let input = [1, 5, 3, 2, 4, 8, 9, 7, 6]
-let output = [1, 5, 3, 2, 4, 8, 9, 7, 6];
+let [...output] = input;
 // 排序
 bubbleSort(output);
 
