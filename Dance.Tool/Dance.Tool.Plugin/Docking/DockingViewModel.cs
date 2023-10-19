4ee9085b77d6d7fc3fb68d5f7e5c0b18f91a2ee5
240829120 <240829120@qq.com>
2023-10-19 18:21:12 +0800

    添加docking样式

---
+++ Dance.Tool/Dance.Tool.Plugin/Docking/DockingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dance.Tool.Plugin
+{
+    /// <summary>
+    /// Docking 视图模型
+    /// </summary>
+    [DanceSingleton]
+    public class DockingViewModel : DanceViewModel
+    {
+    }
+}
