94055243c393ea0f0f4dda47760b8fa3cdd51625
240829120 <240829120@qq.com>
2023-11-17 17:49:07 +0800

    添加propertygrid示例

--- Dance.Tool/Dance.Tool.Plugin/Control/ControlView.xaml.cs
+++ Dance.Tool/Dance.Tool.Plugin/Control/ControlView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Dance.Wpf;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,89 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
 namespace Dance.Tool.Plugin
 {
+    [ExpandableObject]
+    public class Lession
+    {
+        [Category("基础"), Description("描述描述123")]
+        public string? Name { get; set; }
+
+        [Category("基础"), Description("描述描述123")]
+        public int Age { get; set; }
+
+
+    }
+
+    [ExpandableObject]
+    public class Teacher
+    {
+
+        [Category("基础"), Description("描述描述123")]
+        public string? Name { get; set; }
+
+        [Category("基础"), Description("描述描述123")]
+        public int Age { get; set; }
+
+        [Category("基础"), Description("描述描述1231111")]
+        public List<Lession> Lessions { get; set; } = new() { new Lession { Name = "t1" } };
+
+
+        [Category("基础"), Description("描述描述1231111")]
+        public Lession Lession2 { get; set; } = new Lession();
+    }
+
+    public class Student
+    {
+        [Category("基础"), Description("描述描述123")]
+        public string? Property_1_1 { get; set; }
+
+
+        [Category("基础")]
+        public int Property_1_2 { get; set; }
+
+
+        [Category("基础")]
+        public string? Property_1_3 { get; set; }
+
+        [Category("基础")]
+        public string? Property_1_4 { get; set; }
+
+
+        [Category("基础2")]
+        public System.Windows.Media.SolidColorBrush Brush { get; set; }
+
+        [Category("基础2")]
+        public Thickness Thickness { get; set; } = new Thickness(1, 2, 3, 4);
+
+        [Category("基础2")]
+        public DateTime DateTime { get; set; }
+
+        [Category("基础2")]
+        public TimeSpan TimeSpan { get; set; }
+
+        [Category("基础2")]
+        public System.Windows.Media.Color Color { get; set; }
+
+        [Category("老师")]
+        public Teacher Teacher { get; set; } = new();
+
+        [Category("列表")]
+        public List<Teacher> Items { get; set; } = new() { new Teacher { Name = "zs", Age = 17 } };
+
+        [Category("列表")]
+        public List<string> Items2 { get; set; } = new();
+
+        [Category("列表")]
+        public List<int> Items3 { get; set; } = new List<int>() { 1, 2, 3 };
+
+
+        [Category("测试")]
+        public HorizontalAlignment HorizontalAlignment { get; set; }
+    }
+
     /// <summary>
     /// ControlView.xaml 的交互逻辑
     /// </summary>
@@ -28,6 +109,8 @@ namespace Dance.Tool.Plugin
             ControlViewModel vm = DanceDomain.Current.LifeScope.Resolve<ControlViewModel>();
             vm.View = this;
             this.DataContext = vm;
+
+            this.propertyGrid.SelectedObject = new Student();
         }
 
         private void btBlue_Click(object sender, RoutedEventArgs e)
